f18f8ca0fc57304120698e91048d7427f4e74495
Vado42-chris <chris@vado42.ca>
2026-09-14 03:04:01 -0600

    publish cold-start hydration compiler

--- src/cli/public-exports.mjs
+++ src/cli/public-exports.mjs
@@ -6,6 +6,7 @@ import * as Wakes from '../wakes/envelope.mjs';
 import * as Ibal from '../ibal/canary.mjs';
 import * as ImpactFormation from '../ibal/impact-formation.mjs';
 import * as RootProjection from '../ibal/root-projection.mjs';
+import * as ColdStartHydration from '../ibal/cold-start-hydration.mjs';
 import * as Cadence from '../cadence/continuation.mjs';
 import * as SelfDrive from '../cadence/self-drive.mjs';
 import * as Lexicon from '../lexicon/resolve-token.mjs';
@@ -19,6 +20,7 @@ const modules = [
   ['@xi-io/sdk/ibal', Ibal],
   ['@xi-io/sdk/ibal/impact-formation', ImpactFormation],
   ['@xi-io/sdk/ibal/root-projection', RootProjection],
+  ['@xi-io/sdk/ibal/cold-start-hydration', ColdStartHydration],
   ['@xi-io/sdk/cadence', Cadence],
   ['@xi-io/sdk/cadence/self-drive', SelfDrive],
   ['@xi-io/sdk/command-lexicon/resolve', Lexicon],
@@ -32,6 +34,7 @@ const arity = {
   compileIbalCanary: [1, 1],
   compileImpactFormation: [1, 1],
   compileIbalRootProjection: [1, 1],
+  compileColdStartHydration: [1, 1],
   compileContinuationCycle: [1, 1],
   compileContinuationDirective: [1, 1],
   compileContinuationLoop: [1, 1],
